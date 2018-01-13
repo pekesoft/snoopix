a1fc69dbe24818798a2eb26ead0923239c426ecf
ChuxMan <chuxman@pekesoft.com>
2018-01-13 08:34:17 +0100

    Cambios en la estructura y avances con el código. Empezamos a limpiar la parte de investigación y convertirlo en un desarrollo más decente.
    En próximas ediciones, tanto los comentarios del código como los comentarios
    del propio Git van a pasar a traducirse y mantenerse en inglés y adjuntar
    la licencia GPL.

--- wizards/snoopix-setup.sh
+++ wizards/snoopix-setup.sh
@@ -1,7 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 #----------------------------------------------------------------------
 # Script para la instalación y mantenimiento del propio Snoopix.
 #----------------------------------------------------------------------
 # Copyright © 2018 by Peke Soft, Ltd. JAN-2018.
 #----------------------------------------------------------------------
+
+#Comprobamos el Sistema Operativo y su versión.
+#TODO
+
+#Comprobamos la existencia de GIT.
+echo "Introduce el nombre del paquete"
+read miprograma
+RESPUESTA=$(dpkg --get-selections | grep -w ${miprograma} | grep -w install)
+if [ "$RESPUESTA" = "" ]; then
+    echo "NO ESTA INSTALADO"
+    apt -y install ${miprograma}
+else
+    echo "${miprograma} YA ESTA INSTALADO"
+fi
