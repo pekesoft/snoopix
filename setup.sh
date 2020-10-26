e0625228e1de4fdf55e408b5c6dcd54516b945e9
ChuxMan <chuxman@pekesoft.com>
2020-10-26 04:30:02 +0100

    Cambios en el setup y seguimos ampliando framework

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------
+# [EN] System Info Script
+# [ES] Script de Información del Sistema.
+#----------------------------------------------------------------------
+# Copyright © 2020 by Peke Soft, Ltd. Licensed under CC-NC-SA
+#   v.1.0.0 Oct 2020 By ChuxMan. (chuxman@pekesoft.com)
+#----------------------------------------------------------------------
+# Muestra tanto información del sistema, software, hardware y snoopix.
+#----------------------------------------------------------------------
+
+# Little portion of ANSI colors constants defined 
+Red='\033[0;31m'; Green='\033[0;32m'; NC='\033[0m'
+
+#Comprobar la ruta. Si no es /opt/snoopix o /srv/snoopix comprobar dichas rutas y si no crearlo.
+
+
+# Agregar el PATH de snoopix.
+
+# Comprobación de la instancia local.
+# Comprobar /opt/snoopix
+
+# Comprobación de la instancia de servidor.
+# Comprobar /srv/snoopix
+
+# Componentes mínimos del sistema. Utilizamos el framework para instalación multiplataforma.
+
+# Iniciar asistente de sistema básico (IP, nombre máquina, etc)
